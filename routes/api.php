9d2f469bfa7b36b33c513482b99721917478a4db
auth-menn <auth.menn@gmail.com>
2025-06-02 17:50:08 +0700

    update api

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\TicketController;
 
 route::post('/login', [AuthController::class, 'login']);
@@ -11,8 +12,12 @@ route::post('/register', [AuthController::class, 'register']);
 route::middleware('auth:sanctum')->group(function () {
     route::get('/me', [AuthController::class, 'me']);
     route::post('/logout', [AuthController::class, 'logout']);
+
+    Route::get('/dashboard/statistics', [DashboardController::class, 'getStatistics']);
     
     Route::get('/ticket', [TicketController::class, 'index']);
+    Route::get('/ticket/{code}', [TicketController::class, 'show']);
     Route::post('/ticket', [TicketController::class, 'store']);
+    Route::post('/ticket-reply/{code}', [TicketController::class, 'StoreReply']);
 
 });
